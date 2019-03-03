6c49eecffb40c7ed1b4a4e3de83dc3ae2797d6f2
Francisco Roca <frrocaguilar@gmail.com>
2019-03-03 19:14:54 +0800

    ordenacion burbuja

--- ordenacion_python/ordenacion_insercion.py
+++ ordenacion_python/ordenacion_insercion.py
@@ -1,1 +1,16 @@
-#test
+# algoritmo de ordenacion
+def insertionSort(alist):
+    for i in range(1, len(alist)):
+
+        value = alist[i]
+        index = i
+
+        while index > 0 and alist[index - 1] > value:
+            alist[index] = alist[index - 1]
+            index = index - 1
+
+        alist[index] = value
+
+alist = [12, 34, 45, 23, 54, 23, 25]
+insertionSort(alist)
+print(alist)
