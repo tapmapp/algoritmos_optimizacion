5fe5100b638085f6b31cfbd94af48f6130f7b02d
Francisco Roca <frrocaguilar@gmail.com>
2019-03-04 04:42:36 +0800

    reinas

--- AG1/algoritmos_voraces/algoritmo_voraz.py
+++ AG1/algoritmos_voraces/algoritmo_voraz.py
@@ -1,19 +1,48 @@
-sistema = [25, 10, 5 , 1]
+#reinas
+N = 4
+solucion = [0 for i in range(N)]
+etapa = 0
 
-def cambio_monedas(cantidad, sistema):
+def es_prometedora(solucion, etapa):
   
-  solucion = [0 for i in range(len(sistema))]
+  for i in range(etapa + 1):
+    if solucion.count(solucion[i]) > 1:
+      return False
+    
+    # verificar diagonales
+    for j in range(i + 1, etapa + 1):
+      if abs(i - j) == abs(solucion[i] - solucion[j]):
+        return False
+      
+  return True
+
+def escribe(solucion):
+  n = len(solucion)
+  for x in range(n):
+    print("")
+    for i in range(n):
+      if solucion[i] == x + 1:
+        print(" X ", end = "")
+      else:
+        print(" - ", end = "")
+
+def reinas(N, solucion, etapa):
   
-  valorAcumulado = 0
+  for i in range(1, N+1):
+    solucion[etapa] = i
   
-  for i in range(len(sistema)):
+    if es_prometedora(solucion, etapa):
+      if etapa == N-1:
+        # ultima etapa, imprimimos solucion
+        print("\n\nLa solución es:")
+        print(solucion)
+        escribe(solucion)
+      else:
+        # no es prometedora
+        reinas(N, solucion, etapa + 1)
+    else:
+      # no es prometedora
+      None
+    solucion[etapa] = 0
     
-    monedas = int((cantidad  - valorAcumulado) / sistema[i])
-    solucion[i] = monedas
-    
-    valorAcumulado += monedas * sistema[i]
-    
-    if valorAcumulado == cantidad:
-      return solucion
-    
-cambio_monedas(100, sistema)
+reinas(N, solucion, etapa)
